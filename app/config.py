84ec963fa2b77736c13686aba6b20bbc8c2f14cf
MahiFaiyaz <faiyaz1997@gmail.com>
2025-03-15 18:49:26 -0400

    updated config file

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 GOOGLE_SHEETS_API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
