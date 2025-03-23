3505b2e945f6c00d0c86ab58963b0f16699c9a3d
MahiFaiyaz <faiyaz1997@gmail.com>
2025-03-23 05:02:54 -0400

    Updated main.py to import api routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+
 from app.routers import sync, manhwa_finder
 
-app = FastAPI()
+app = FastAPI(
+    title="Manhwa Finder API",
+    description="An API for fetching and filtering manhwa details",
+    version="1.0",
+)
 
+
+# Add a root route
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Manhwa Finder API!"}
+
+
+# Include routers
 app.include_router(manhwa_finder.router)
-app.include_router(sync.router)  # This adds the /sync route
+# This adds routes from sync.py to the app
+app.include_router(sync.router)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
