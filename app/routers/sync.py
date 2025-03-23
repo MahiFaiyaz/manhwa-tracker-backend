5143af4da1e28b10a99163b63e56e08887f898ca
MahiFaiyaz <faiyaz1997@gmail.com>
2025-03-23 05:27:25 -0400

    Added api key for sync API

--- app/routers/sync.py
+++ app/routers/sync.py
@@ -1,15 +1,22 @@
-from fastapi import APIRouter, BackgroundTasks
-from app.services.manhwa_database_sync import ManhwaSync
-from app.services.google_sheets_manager import GoogleSheetsManager
+from fastapi import APIRouter, BackgroundTasks, HTTPException, Header
+from app.config import SYNC_API_KEY
+
 
 router = APIRouter()
 
+SECRET_API_KEY = SYNC_API_KEY
+
 
 @router.post("/sync")
-async def sync(background_tasks: BackgroundTasks):
+async def sync(background_tasks: BackgroundTasks, api_key=Header(None)):
     # Run both fetch_all and sync_all methods in the background
+    if api_key != SECRET_API_KEY:
+        raise HTTPException(status_code=403, detail="Forbidden: Invalid API Key")
 
     def sync_task():
+        from app.services.manhwa_database_sync import ManhwaSync
+        from app.services.google_sheets_manager import GoogleSheetsManager
+
         # Create instances of ManhwaDataManager and ManhwaSync
         data_manager = GoogleSheetsManager()
         syncer = ManhwaSync()
